dbe3f96ca7832682a05416d37a2c296cb2f98138
rishabh parashar <rishabh.parashar8@gmail.com>
2018-10-30 13:38:45 +0530

    Create Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		try {
+			ArrayList<BigInteger> arr = new ArrayList<BigInteger>();
+
+			BigInteger sum = BigInteger.valueOf(0);
+			long n = 100;
+			BigInteger t1 = BigInteger.valueOf(0);
+			BigInteger t2 = BigInteger.valueOf(1);
+
+			for (long i = 1; i <= n; i++) {
+				//System.out.println(t1);
+				sum = t1.add(t2);
+				t1 = t2;
+				t2 = sum;
+
+				if (sum.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0))) {
+					if(sum.compareTo(BigInteger.valueOf(4000000)) ==-1){
+						arr.add(sum);
+					}
+					
+				}
+			}
+			System.out.println(arr);
+			BigInteger total = BigInteger.valueOf(0);
+			for(int j =0;j<arr.size();j++)
+			{
+				total.add(arr.get(j));
+			}
+			System.out.println(total);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
